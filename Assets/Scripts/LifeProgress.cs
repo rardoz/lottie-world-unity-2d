9687297f58770d50aabeede1f642a1763db70f40
Roe Greene <robert@byrojo.com>
2022-01-12 05:53:08 -0700

    add reset

--- Assets/Scripts/LifeProgress.cs
+++ Assets/Scripts/LifeProgress.cs
@@ -7,11 +7,17 @@ using UnityEngine.SceneManagement;
 public class LifeProgress : MonoBehaviour
 {
 
+    void Reset()
+    {
+        Life.totalLives = Life.maxLives;
+        transform.localScale = new Vector2(1.0f, transform.localScale.y);
+    }
     // Update is called once per frame
     void Update()
     {
         if (transform.localScale.x <= 0)
         {
+            Reset();
             SceneManager.LoadScene("game-over"); //Load scene called Game
         }
         else
