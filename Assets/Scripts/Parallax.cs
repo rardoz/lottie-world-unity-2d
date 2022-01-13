ca45db4e298aab5e2cac37fd00c5cd1bf0e5a873
Roe Greene <robert@byrojo.com>
2022-01-13 03:43:12 -0700

    ingore for now

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+    public Transform subject;
+    Vector2 startPosition;
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - (startPosition);
+
+    float distanceFromSubject => transform.position.z - (subject.position.z);
+
+    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    float parallaxFactor => Mathf.Abs(distanceFromSubject / clippingPlane);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = cam.ScreenToWorldPoint(transform.position) * (transform.position.z);
+        startZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = (startPosition + travel * parallaxFactor); /// 2.5f;
+        transform.position = new Vector3(newPos.x, transform.position.y, startZ);
+        //  transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
--- Assets/Scripts/items/ParallaxBackground.cs
+++ Assets/Scripts/items/ParallaxBackground.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
 
-	public Vector2 parallaxEffectMultiplier;
-	private Transform cameraTransform;
-	Vector3 lastCameraPosition;
+    public Vector2 parallaxEffectMultiplier;
+    private Transform cameraTransform;
+    Vector3 lastCameraPosition;
 
-	private void Start()
-	{
-		cameraTransform = Camera.main.transform;
-		lastCameraPosition = cameraTransform.position;
-	}
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
 
-	private void Update()
-	{
-		Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-		transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
-		lastCameraPosition = cameraTransform.position;
-	}
+    private void Update()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+    }
 }
+
+
