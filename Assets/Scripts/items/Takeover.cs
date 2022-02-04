2b75e12a8989be977c740eeb83ea2c1f95d88c04
Roe Greene <robert@byrojo.com>
2022-02-03 23:10:43 -0600

    add backgroud music to intro scenes

--- Assets/Scripts/items/Takeover.cs
+++ Assets/Scripts/items/Takeover.cs
@@ -6,6 +6,10 @@ public class Takeover : LevelStart
 {
     public bool shouldGoToNextLevel = false;
 
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("BackgroundAudio").GetComponent<AudioSource>().Stop();
+    }
     // Update is called once per frame
     void Update()
     {
