c918bb082d2ddc65eb77a6b5a7e070ae63610e3a
Roe Greene <robert@byrojo.com>
2022-02-03 23:25:35 -0600

    add music to game over

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,14 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    bool didPressDown = false;
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        // insure a full press so we don't accidently skip the game over screen when we die
+        if (Input.GetButtonUp("Jump") && didPressDown)
         {
             SceneManager.LoadScene("C1S1");
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            didPressDown = true;
         }
     }
 }
