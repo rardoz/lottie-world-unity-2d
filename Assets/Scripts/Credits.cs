5b05285248eb1f4d6de9e270c8a1633b10aecd94
Roe Greene <robert@byrojo.com>
2022-02-04 22:37:26 -0600

    add exit after credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+   bool creditsFinished = false;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        // insure a full press so we don't accidently skip the game over screen when we die
+        if (Input.GetButtonUp("Jump") && creditsFinished)
+        {
+            SceneManager.LoadScene("C1S1Intro");
+        }
+    }
+
+    public void CreditFinishAnimationEvent()
+    {
+        creditsFinished = true;
+    }
+}
