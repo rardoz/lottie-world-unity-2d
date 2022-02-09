07ec4210246b6577b7f820858b01340c8c024f97
Roe Greene <robert@byrojo.com>
2022-02-08 21:39:21 -0600

    close bear traps

--- Assets/Scripts/Deadly/BearTrap.cs
+++ Assets/Scripts/Deadly/BearTrap.cs
@@ -7,6 +7,13 @@ public class BearTrap : Death
     public override bool ShouldKill(Collider2D c2d)
     {
         bool isPlayer = c2d.CompareTag("Player");
+        if(isPlayer) {
+            GetComponent<Animator>().SetBool("Close", true);
+        }
         return isPlayer;
+    }
+
+    public void KillAnimationEvent() {
+        Destroy(gameObject, destoryDelay);
     }
 }
